2ab5daf4f692f4b75040b4014debb13700423817
Gianni <gianni.harre@gmx.de>
2025-09-08 18:03:21 +0200

    Giannicorp Gym V1.0.0

---
+++ src/capacitor.config.ts
@@ -0,0 +1,10 @@
+// capacitor.config.ts
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.giannicorp.fitness',
+  appName: 'GianniCorpFitness',
+  webDir: 'dist'
+};
+
+export default config;
